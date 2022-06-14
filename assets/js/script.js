4765ff5e617251eb1e6058dd7a0a6d716fe8d179
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-06-14 19:28:08 +0000

    add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id  
@@ -11,5 +12,25 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 }))
 
 function generatecomputerChoice() {
+const randomNumber = Math.floor(Math.random() * 3) + 1
+if (randomNumber === 1) {
+  computerChoice = 'rock'
+}
+if (randomNumber === 2) {
+  computerChoice = 'scissors'
+}
+if (randomNumber === 3) {
+  computerChoice = 'paper'
+}
+computerChoiceDisplay.innerHTML = computerChoice
+}
 
+function getResult () {
+if (userChoice === computerChoice) {
+resultDisplay.innerHTML = 'Its a draw!'
+} else if (userChoice === 'rock' && computerChoice === 'paper' ) {
+  resultDisplay.innerHTML = 'You Lose!'
+} else if (userChoice === 'rock' && computerChoice === 'Scissors' ) {
+  resultDisplay.innerHTML = 'You Lose!'
+}
 }
